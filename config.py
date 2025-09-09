157d25bb9a77f2d92f8de18c41d9bb11a4cbec2a
⎯᪵⎯꯭̽🚬᪵᪳ ⃪꯭𝐁ʌ‌꯭ᴅ꯭𝐍꯭ᴀ‌꯭ᴍ꯭ ꯭🖤᪵᪳᪳𝆺꯭𝅥⎯꯭̽⟶᯦꯭ <rejemtahir@gmail.com>
2025-09-09 15:42:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ITSZSHUKLA")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MASTIWITHFRIENDSXD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/kriti_bot_update")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KRITI_SUPPORT_GROUP")
 
 # YT-API Get from @AmShashank on tg
 YOUR_API_KEY = getenv("YOUR_API_KEY", "")
