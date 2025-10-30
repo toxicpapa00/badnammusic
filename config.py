4ea2ff32fac60e30b2de9c31995c169fd4fc2ecb
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-10-30 14:56:03 +0530

    Update START_IMG_URL to a new image source

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ confirmer = {}
 DEBUG_IGNORE_LOG =True
 
 # Image URLs
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/b61227af05544deb76a34.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/o4e6gc.jpg")
 PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/7bb907999ea7156227283.jpg")
 PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
